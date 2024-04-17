5909c7611996100685393569a0e0e215a671306b
ann-era <148546178+ann-era@users.noreply.github.com>
2024-04-17 23:23:26 +0800

    Add files via upload

---
+++ Mock  Up Queue System/validityCheck.js
@@ -0,0 +1,9 @@
+function validityChecker(event) {
+    if (!document.getElementById('PhoneNumber').checkValidity()) {
+        alert('Invalid phone number format. Please enter a valid Malaysian phone number.');
+        event.preventDefault();
+    } else {
+        event.preventDefault();
+        window.location.href = "Page3.html";
+    }
+}
